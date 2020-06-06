fd78eee5965e4279dc1dc4055cd4666be7c013fc
ChanMinKim <rlacksals96@gmail.com>
2020-06-06 13:39:29 +0900

    Update WarGameClient.java

--- WarGameClient.java
+++ WarGameClient.java
@@ -14,17 +14,17 @@ public class WarGameClient {//handle connection and use rmi function here from c
 		
 		System.out.println("Remote Method Invocate to "+mServer+", service name: "+mServName);
 		new WarGameClient();
-//		try {
-//			WarGame c=(WarGame)Naming.lookup("rmi://"+mServer+"."+mServName);
-//			c.func();
-//		}catch(MalformedURLException mue) {
-//			System.out.println("MalformedURLException: "+mue);
-//		}catch(RemoteException re) {
-//			System.out.println("RemoteException: "+re);
-//		}catch(NotBoundException nbe) {
-//			System.out.println("NotBoundException: "+nbe);
-//		}catch(java.lang.ArithmeticException ae) {
-//			System.out.println("java.lang.ArithmeticException: "+ae);
+		try {
+			WarGame c=(WarGame)Naming.lookup("rmi://"+mServer+"."+mServName);
+			c.func();
+		}catch(MalformedURLException mue) {
+			System.out.println("MalformedURLException: "+mue);
+		}catch(RemoteException re) {
+			System.out.println("RemoteException: "+re);
+		}catch(NotBoundException nbe) {
+			System.out.println("NotBoundException: "+nbe);
+		}catch(java.lang.ArithmeticException ae) {
+			System.out.println("java.lang.ArithmeticException: "+ae);
 //		}
 	}
 }
