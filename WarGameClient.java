66ae87f7a60399cf7899cc340997dd1a73b9c234
LeeJooHwan <tim2875@naver.com>
2020-06-13 19:32:53 +0900

    Add files via upload

--- WarGameClient.java
+++ WarGameClient.java
@@ -1,99 +1,58 @@
 import java.rmi.AccessException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.Scanner;
-import java.net.MalformedURLException;
-import java.rmi.NotBoundException;
-import java.net.MalformedURLException;
-import java.rmi.NotBoundException;
-public class WarGameClient implements Runnable {//handle connection and use rmi function here from client side
-//	private static final long serialVersionUID = 1L;
-	private String id;
-	boolean chkLog=false;
-	private WarGame server;
-	boolean boolean_ReadyBtnStatus;
-	boolean boolean_HitBtnStatus;
-	boolean boolean_DropBtnStatus;
-	
-	protected WarGameClient(WarGame wg,String id,String pw){
-		boolean_ReadyBtnStatus=false;
-		boolean_HitBtnStatus=false;
-		boolean_DropBtnStatus=false;
-		this.server=wg;
-		this.id=id;
-		try {
-			chkLog=server.checkClientCredentials(wg, id, pw);
-			if(chkLog==false) {
-				System.out.println("wrong id or wrong passworld");
-				System.exit(0);
-			}
-			
-		
-		}catch(RemoteException re) {
-			System.out.println("RemoteException: "+re);
-		
-		}catch(java.lang.ArithmeticException ae) {
-			System.out.println("java.lang.ArithmeticException: "+ae);
-		}
-		
-	}
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.RMIClientSocketFactory;
 
-	public void run() {
-		if(chkLog) {
-			System.out.println(id);
-			WarGameGUI gui=new WarGameGUI(id);
-			try {
-				while(server.checkAllReady()==false) {
-					boolean_ReadyBtnStatus=gui.returnReadyBtnStatus();
-					server.setReadyStatus(id, boolean_ReadyBtnStatus);	
-				}
-				
-			}catch(Exception e) {//디테일하게 예외 잡아 줘야 할 듯.. 예외처리 사이즈가 
-				e.printStackTrace();
-			}
-			System.out.println("game start");//this will be deleted in the end
-			gui.setGameInfo("game start");
-			gui.deactivateReadyBtn();
-			
-//			try {
-//				do{//게임중... do while인 이유는 마지막 카드가 공격이어서 기회가 남아 있을수도 있다..
-//					////////////////////
-//				}while(server.checkStatus());
-//			}catch(Exception e) {//디테일하게 예외 잡아 줘야 할 듯..예외처리 사이즈가 커 
-//				e.printStackTrace();
-//			}
-		}
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.rmi.NotBoundException;
+
+public class WarGameClient  {//handle connection and use rmi function here from client side
+	private static final int PORT = 2019;
+	
+	
+	public WarGameClient() {
+		new WarGameGUI();
 	}
 	public static void main(String[] args) {
-		String mServer="127.0.0.1";
-//		String mServer=args[0];
+		String mServer="localhost";
 		String mServName="WarGame";
 		
 		System.out.println("Remote Method Invocate to "+mServer+", service name: "+mServName);
 		
-		try {
+		try { 	
+			System.setProperty("javax.net.ssl.trustStore", "trustedcerts");
+			System.setProperty("javax.net.ssl.trustStorePasssword", "123456");
 			
-			String id;
-			String pw;
-			Scanner sc=new Scanner(System.in);//this is for temporary
-			System.out.println("enter id: ");
-			id=sc.nextLine();
-			System.out.println("enter pw: ");
-			pw=sc.nextLine();
 			
-			WarGame wg=(WarGame)Naming.lookup("rmi://"+mServer+":1099/"+mServName);
-			new Thread(new WarGameClient(wg,id,pw)).start();
+			Registry registry = LocateRegistry.getRegistry(
+	                InetAddress.getLocalHost().getHostName(), PORT,
+	                new RMISSLClientSocketFactory());	
 			
-		}catch(MalformedURLException mue) {
-			System.out.println("MalformedURLException: "+mue);
-		}catch(RemoteException re) {
-			System.out.println("RemoteException: "+re);
+			WarGame c=(WarGame)registry.lookup("WarGame"/*"rmi://"+mServer+":1099/"+mServName*/);
+			
+			c.func();
+			new WarGameClient();
+			System.out.print("실행함 : : "); c.func();
+			
+			
+			System.out.println("????");
+		}catch(IOException e) {
+			System.out.println("IOException :" +e);
 		}catch(NotBoundException nbe) {
 			System.out.println("NotBoundException: "+nbe);
 		}catch(java.lang.ArithmeticException ae) {
 			System.out.println("java.lang.ArithmeticException: "+ae);
 		}
-	}
 	
+	}
 }
--- WarGameServer.java
+++ WarGameServer.java
@@ -1,140 +1,41 @@
-import java.net.*;
-import java.nio.channels.ServerSocketChannel;
-import java.util.ArrayList;
 import java.io.*;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-import java.rmi.Naming;
+public class WarGameServer extends UnicastRemoteObject{
+	
+	 private static final int PORT = 2019;
+	
+	public WarGameServer() throws Exception{
+		super(PORT,
+	          new RMISSLClientSocketFactory(),
+	          new RMISSLServerSocketFactory());
+	}
+	
+    public static void main(String args[]) {
 
-public class WarGameServer {
-	public int clientCnt=0;
-//	private WarGameServerRunnable client[]=new WarGameServerRunnable[2];
-	public WarGameServer(String server,String servName) {
-		try {
-			WarGame wg=new WarGameImpl();
-			Naming.rebind("rmi://"+server+":1099/"+servName, wg);
-		}catch(Exception e) {
-			System.out.println("Trouble: "+e);
-		}
-	}
-	public static void main(String[] args) throws UnknownHostException {
-//		String mServer=InetAddress.getLocalHost().getHostName();
-//		String mServer=args[0];
-		String mServer="127.0.0.1";
-		String mServName="WarGame";
-		System.out.println("started at"+mServer+"and use default port(1099). service name: "+mServName);
-		new WarGameServer(mServer,mServName);
-		
-		
-	}
+        try {
+    		System.setProperty("javax.net.ssl.trustStore", "trustedcerts");
+			System.setProperty("javax.net.ssl.trustStorePasssword", "123456");
+			
+        	
+            // Get SSL-based registry
+        	Registry registry = LocateRegistry.createRegistry(PORT, new RMISSLClientSocketFactory(), new RMISSLServerSocketFactory());
+
+            //Create new impl object
+        	WarGameImpl warGameImpl = new WarGameImpl();
+
+            registry.bind("WarGame", warGameImpl);
+
+            System.out.println("WarGame bound in registry");
+
+        } catch (Exception e) {
+            System.out.println("WarGame Error : " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
-//	public int whoClient(int clientID) {
-//		for (int i = 0; i < clientCnt; i++)
-//			if (client[i].getClientID() == clientID)
-//				return i;
-//		return -1;
-//	}
-//	public void putClient(int clientID, String inputLine) {
-//		for (int i = 0; i < clientCnt; i++)
-//			if (client[i].getClientID() == clientID) {
-//				System.out.println("writer: "+clientID);
-//			} else {
-//				System.out.println("write: "+client[i].getClientID());
-//				client[i].out.println(inputLine);
-//			}
-//	}
-	//this is about chatting. so..
-//	public void addClient(ServerSocket serverSocket) {
-//		Socket clientSocket = null;
-//		
-//		if (clientCnt < client.length) { 
-//			try {
-//				clientSocket = serverSocket.accept();
-//				clientSocket.setSoTimeout(40000); // 1000/sec
-//			} catch (IOException i) {
-//				System.out.println ("Accept() fail: "+i);
-//			}
-//			client[clientCnt] = new WarGameServerRunnable(this, clientSocket);
-//			new Thread(client[clientCnt]).start();
-//			clientCnt++;
-//			System.out.println ("Client connected: " + clientSocket.getPort()
-//					+", CurrentClient: " + clientCnt);
-//		} else {
-//			try {
-//				Socket dummySocket = serverSocket.accept();
-//				WarGameServerRunnable dummyRunnable = new WarGameServerRunnable(this, dummySocket);
-//				new Thread(dummyRunnable);
-//				dummyRunnable.out.println(dummySocket.getPort()
-//						+ " < Sorry maximum user connected now");
-//				System.out.println("Client refused: maximum connection "
-//						+ client.length + " reached.");
-//				dummyRunnable.close();
-//			} catch (IOException i) {
-//				System.out.println(i);
-//			}	
-//		}
-//	}
-//	public synchronized void delClient(int clientID) {
-//		int pos = whoClient(clientID);
-//		WarGameServerRunnable endClient = null;
-//	      if (pos >= 0) {
-//	    	   endClient = client[pos];
-//	    	  if (pos < clientCnt-1)
-//	    		  for (int i = pos+1; i < clientCnt; i++)
-//	    			  client[i-1] = client[i];
-//	    	  clientCnt--;
-//	    	  System.out.println("Client removed: " + clientID
-//	    			  + " at client[" + pos +"], CurrentClient: " + clientCnt);
-//	    	  endClient.close();
-//	      }
-//	}
-//}
-//class WarGameServerRunnable implements Runnable{
-//	protected WarGameServer warGameServer=null;
-//	protected Socket clientSocket=null;
-//	protected PrintWriter out=null;
-//	protected BufferedReader in=null;
-//	public int clientID=-1;
-//	
-//	Game game=new Game();
-//	ArrayList<Card> deck0=null;//내려놓는 곳 
-//	ArrayList<Card> deck1;//p1 deck
-//	ArrayList<Card> deck2;//p2 deck
-//	int playerCardCnt[]=new int[2];//player의 카드 갯
-//	int deck0Cnt;//내려놓는 곳 카드 수 
-//	int turn=0;
-//	public WarGameServerRunnable(WarGameServer server,Socket socket){
-//		this.warGameServer=server;
-//		this.clientSocket=socket;
-//		clientID=clientSocket.getPort();
-//		try{
-//			out=new PrintWriter(clientSocket.getOutputStream(),true);
-////			out=new BufferedWriter(clientSocket.getOutputStream());
-//			in=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//		}catch(IOException ioe){}
-//	}
-//	public void run() {
-//		gameInit();
-//	}
-//	public void close() {
-//		try {
-//			if(in !=null) in.close();
-//			if(out !=null) out.close();
-//			if(clientSocket!=null)clientSocket.close();
-//			
-//		}catch(IOException i) {
-//			i.printStackTrace();
-//		}
-//	}
-//	public int getClientID() {
-//		return clientID;
-//	}
-//	public void gameInit() {
-//
-//		deck1=game.returnDeck1();//이미 셔플된 상태로 나온거라 따로 안해줘도 
-//		deck2=game.returnDeck2();
-//		playerCardCnt[0]=deck1.size();
-//		playerCardCnt[1]=deck2.size();
-//		deck0Cnt=0;
-//		
-//	}
+
 
